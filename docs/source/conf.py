3694bb18e2db0564da76f2c6784a8848d6268ff0
Hack FB Password and ID <91569582+hoseobrez@users.noreply.github.com>
2021-09-30 22:50:56 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'howtohackfacebook'
+copyright = '2021, howtohackfacebook'
+author = 'howtohackfacebook'
 
 release = '0.1'
 version = '0.1.0'
